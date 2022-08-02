79d07d1261c29117adf537d97077551d6205db8c
Farid0558475484 <109238744+Farid0558475484@users.noreply.github.com>
2022-08-02 18:41:07 +0400

    homework12

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+            
+        {
+           
+            for (int i = 1; i < 10000; i++)
+
+            {
+                if(i % 7==0);
+            Console.WriteLine(i);
+            }
+        }
+    }
+}
